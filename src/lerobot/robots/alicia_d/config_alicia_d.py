d13da39af235c522b010a22f288a720aa7feb7b6
royalAkira <1612731237@qq.com>
2025-11-12 11:58:55 +0800

    fix config

--- src/lerobot/robots/alicia_d/config_alicia_d.py
+++ src/lerobot/robots/alicia_d/config_alicia_d.py
@@ -29,10 +29,10 @@ class AliciaDConfig(RobotConfig):
     def default_cameras_config() -> dict[str, CameraConfig]:
         return {
             "wrist": OpenCVCameraConfig(
-                index_or_path="/dev/video0", fps=30, width=640, height=480, rotation=Cv2Rotation.ROTATE_90
+                index_or_path="/dev/video0", fps=30, width=480, height=640, rotation=Cv2Rotation.ROTATE_90
             ),
             "front": OpenCVCameraConfig(
-                index_or_path="/dev/video2", fps=30, width=640, height=480, rotation=Cv2Rotation.ROTATE_90
+                index_or_path="/dev/video2", fps=30, width=480, height=640, rotation=Cv2Rotation.ROTATE_90
             ),
         }
     # 串口/波特率
